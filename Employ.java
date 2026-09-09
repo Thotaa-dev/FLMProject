564784974b6e81ef2e0fdbd1dbb8f468af70ade5
Thota <mahenderthotaa@gmail.com>
2026-09-09 11:32:44 +0530

    class closed

--- Employ.java
+++ Employ.java
@@ -1,5 +1,7 @@
 public class Employee{
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		System.out.println("Hii ...");
+		System.out.println("Byee ......");
 	}
+}
